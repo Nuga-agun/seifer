040401a5930f342737c1a31dcc74a05e09a395cd
Nuga <nuga_agun@proton.me>
2026-08-06 17:50:12 +0200

    feat: swap_bytes

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,5 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn swap_bytes(list: &mut [u8], i: usize, j: usize) {
+    let temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
 }
 
 #[cfg(test)]
@@ -7,8 +9,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn swap_two() {
+        let mut list: [u8;2] = [8,3];
+        swap_bytes(&mut list, 0, 1);
+        assert_eq!(list, [3,8]);
     }
 }
